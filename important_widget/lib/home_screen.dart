96f839c316c797a74d0f4a90a41c9efb1d6830d6
mdmhamudulislamrakib <mdmhamudulislamrakib@gmail.com>
2026-01-05 10:11:13 +0600

    update

--- important_widget/lib/home_screen.dart
+++ important_widget/lib/home_screen.dart
@@ -15,16 +15,53 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(backgroundColor: Colors.green),
       body: Column(
-        children: [
-          Switch(
+        spacing: 15,
+        children:[
+          Switch( 
             value: isConnected,
+            activeColor: Colors.red,
+            activeTrackColor: Colors.blue,
+            inactiveTrackColor: Colors.amber,
+            inactiveThumbColor: Colors.black,
             onChanged: (value) {
               setState(() {
                 isConnected = value;
               });
-            
             },
-           )
+        ),
+
+           SwitchListTile (
+            value:isConnected,
+            onChanged: (value){
+              setState(() {
+                isConnected = value!;
+              });
+             },
+            title: Text("Internet Connection"),
+            subtitle: Text("WiFi or Mobile Data"),
+            secondary: Icon(Icons.wifi),
+            ),
+
+           Checkbox(
+            value: isConnected,
+            onChanged: (value) {
+            setState(() {
+              isConnected = value!;
+            });
+           },
+           ),
+           CheckboxListTile(
+            value: isConnected,
+            onChanged: (value) {
+            setState(() {
+              isConnected = value!;
+            });
+           },
+            title: Text("Accept Terms"),
+            subtitle: Text("Please accept terms and conditions"),
+            secondary: Icon(Icons.info),
+           ),
+           
         ],
       ),
     );
